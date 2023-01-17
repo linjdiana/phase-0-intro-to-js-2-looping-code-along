8f764c6686c9f1127367083d5f079ffb188d8e6f
Diana Lin <linjdiana@gmail.com>
2023-01-16 17:42:14 -0800

    done

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+
+function writeCards(Array, event) {
+    const newArray = [];
+    for (let i = 0; i < Array.length; i++) {
+        newArray.push(`Thank you, ${Array[i]}, for the wonderful ${event} gift!`);
+        debugger;
+    }
+    return newArray;
+}
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number)
+        number--
+    }
+
+}
+
+writeCards(["Guadalupe","Ollie","Aki"], "surprise");
+countDown(4)
